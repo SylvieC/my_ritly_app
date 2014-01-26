1e164e013e816af2dcd223b4a5fa7fcc23b4ed91
SylvieC <sylviecottrell@gmail.com>
2014-01-25 16:58:37 -0800

    fixed routing

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,29 +9,38 @@ def new
 end
 
 def create
-  new_link = params.require(:link).permit(:old_value, :length)
-  if  new_link['length'].to_i == 16
-    new_link['new_value']= SecureRandom.base64
-  else
-    new_link['new_value'] = generate_random_url_of_length(new_link['length'])
+  link_params = params.require(:link).permit(:length)
+  length = link_params[:length]
+   url_params = params.require(:link).permit(:url)
+   url = url_params[:url]
+   random_string = ''
+   if length == nil
+     random_string = SecureRandom.urlsafe_base64(10)
+   else
+      random_string = SecureRandom.urlsafe_base64(length) 
   end
-  @link = Link.create(new_link)
-
+  new_link = Link.new
+  new_link['url'] = url  #is it ok to write instead new_link.url = url....
+  new_link['random_string'] = random_string
+  @link = new_link
   redirect_to links_path(@link.id)
 end
 
 def show
 end
 
-def generate_random_url_of_length (n)
-    answer = SecureRandom.base64(3*(n+1)/4)
-    if n%4 == 0
-       return answer
-    elsif n%4 != 0
-       return answer[4-n%4,answer.length]
-    end
-end 
-
-
-
 end
+
+
+# def go
+#   c = params[:id]
+#   @link = Link.find(id)
+#   url = 'http://' + @link.random_string
+  
+#   redirect_to url
+# end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 MyRitlyApp::Application.routes.draw do
 
 
-	root to: "links#index"
+	root to: "links#index", as: 'links'
  
 get '/links/:id', to: 'links#show'
 post '/links', to: "links#create", as: 'link_create'
+# get '/go/:id', to: "links#go", as: 'final_path'
 
 
 end
