fd668ae063b8872305dc5192ba1d51a9f5aa0eed
SylvieC <sylviecottrell@gmail.com>
2014-01-25 18:35:04 -0800

    finally go the show view working

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -19,26 +19,28 @@ def create
    else
       random_string = SecureRandom.urlsafe_base64(length) 
   end
+  
   new_link = Link.new
   new_link['url'] = url  #is it ok to write instead new_link.url = url....
   new_link['random_string'] = random_string
   @link = new_link
-  redirect_to links_path(@link.id)
+  @link.save 
+  redirect_to show_path(@link.id)
 end
 
 def show
+  id = params[:id]
+  @link = Link.find(id)
+end
+
+
+def go
+  id1 = params[:id]
+  link = Link.find(:id1)
+  id = link.random_string
 end
 
 end
-
-
-# def go
-#   c = params[:id]
-#   @link = Link.find(id)
-#   url = 'http://' + @link.random_string
-  
-#   redirect_to url
-# end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ MyRitlyApp::Application.routes.draw do
 
 	root to: "links#index", as: 'links'
  
-get '/links/:id', to: 'links#show'
+
 post '/links', to: "links#create", as: 'link_create'
-# get '/go/:id', to: "links#go", as: 'final_path'
+get '/links/:id', to: 'links#show', as: 'show'
+# get '/go/:id', to: "", as: 'final_path'
 
 
 end
